faca97d81d29288201b3c7d5053bc8dd7e0ed8f4
Fatima EL ASRI <fatimaelassri041@gmail.com>
2025-04-04 23:13:44 +0000

    correct authorization

--- academicms/src/main/java/pfe/mandomati/academicms/Controller/StudentController.java
+++ academicms/src/main/java/pfe/mandomati/academicms/Controller/StudentController.java
@@ -33,7 +33,7 @@ public class StudentController {
 
     @GetMapping("/profile")
     @PreAuthorize("hasRole('STUDENT')")
-    public ResponseEntity<StudentDto> getStudentProfile(@RequestHeader("Authorisation") String authorizationHeader ) {
+    public ResponseEntity<StudentDto> getStudentProfile(@RequestHeader("Authorization") String authorizationHeader ) {
         String token = authorizationHeader.replace("Bearer ", "");
         StudentDto student = studentService.getStudentByToken(token);
         return ResponseEntity.ok(student);        
